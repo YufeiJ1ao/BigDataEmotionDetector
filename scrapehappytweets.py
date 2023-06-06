36f8c3443e9478a58ae7accf1599cf5d9ff973f3
Yufei Jiao <y.jiao0320211@arts.ac.uk>
2023-06-06 15:15:50 +0100

    Add files via upload

---
+++ scrapehappytweets.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from tqdm.notebook import tqdm
+import snscrape.modules.twitter as sntwitter
+
+scraper = sntwitter.TwitterSearchScraper("#happy")
+
+for i, tweet in enumerate.get_items():
+    break
+tweets= []
+data = [tweet.emotion, tweet.text,]
+
+tweets.append(data)
+if i > 50:
+    break
+tweet_df = pd.DataFrame(
+    tweets, colums = ["emotion","text"]
+)
+tweet_df.to_csv("emotion-dataset.csv", index=False)
